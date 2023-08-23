0fd007a5952913b80f7d0b24b355f6f5f3494ee8
dat <dat.nc200137@sis.hust.edu.vn>
2023-08-23 09:33:07 +0700

    first draft

--- run_counting.py
+++ run_counting.py
@@ -20,13 +20,10 @@ from yolox.exp import get_exp
 from tracker.byte_tracker.byte_tracker import BYTETracker
 from tracker.center_tracker.centroid_tracker import CentroidTracker
 from tracker.center_tracker.trackableobject import TrackableObject
+from utils.video import VideoInfo
 
-# execution start time
-start_time = time.time()
-# setup logger
-logging.basicConfig(level=logging.INFO, format="[INFO] %(message)s")
-logger = logging.getLogger(__name__)
 
+POLYGONS = []
 
 def parse_arguments():
     # function to parse the arguments
@@ -35,18 +32,22 @@ def parse_arguments():
                     help="path to config file")
     ap.add_argument("-f", "--exp_file", type=str, default="yolox/version/mot/yolox_tiny_mix_det.py")
     ap.add_argument("-ckpt", type=str, default="weights/bytetrack_tiny_mot17.pth")
-    ap.add_argument("-i", "--input", type=str,
+    ap.add_argument("-i", "--input", type=str, default=0,
                     help="path to optional input video file")
     ap.add_argument("-o", "--output", type=str,
                     help=" path to optional output video file")
-    ap.add_argument("-n", "--name", type=str,
+    ap.add_argument("-n", "--name", type=str, default=None,
                     help=" path to optional output video file")
+    ap.add_argument("-n", "--name", type=str, default="yolox/version/mot/yolox_tiny_mix_det.py")
+    ap.add_argument("-cp", "--classes_path", type=str, default="classes\coco.yaml")
+    ap.add_argument("-d", "--device", type=str, default="cuda:0")
+
     args = ap.parse_args()
 
     return args
 
 
-def main():
+def main(logger):
     # main function for people_counter.py
     args = parse_arguments()
     exp = get_exp(args.exp_file, args.name)
@@ -73,52 +74,19 @@ def main():
                 sys.exit(1)
             capture = cv2.VideoCapture(args.input)
 
-    # initialize the video writer (we'll instantiate later if need be)
-    writer = None
-
-    # initialize the frame dimensions (we'll set them as soon as we read
-    # the first frame from the video)
-    width = None
-    height = None
-
-
     # Initialize model
     model = exp.get_model()
-
-    model.cuda()
-    model.eval()
 
     # Initialize byte_tracker
     ByteTracker = BYTETracker(args, frame_rate=30)
 
+    # Get video information
+    video_info = VideoInfo.from_video(capture)
+
     # # Initialize tracking
-    track = Tracking(model, ByteTracker, None, args)
-
-    if args.ckpt:
-        logger.info("loading checkpoint")
-        ckpt = torch.load(args.ckpt, map_location="cuda:0")
-        # load the model state dict
-        model.load_state_dict(ckpt["model"])
-        logger.info("loaded checkpoint done.")
-
-    # start the frames per second throughput estimator
-    fps = FPS().start()
-
-    ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
-    trackers = []
-    trackableObjects = {}
-
-    # initialize the total number of frames processed thus far, along
-    # with the total number of objects that have moved either up or down
-    totalFrames = 0
-    totalDown = 0
-    totalUp = 0
-    # initialize empty lists to store the counting data
-    total = []
-    move_out = []
-    move_in =[]
-    out_time = []
-    in_time = []
+    track = Tracking(logger=logger, model=model, video_info=video_info, tracker=ByteTracker, args=args)
+    track.load_model(weights_path=args.ckpt, classes=args.classes_path, device=args.device)
+    track.init_drawer(polygons=POLYGONS)
 
     # loop over frames from the video stream
     while True:
@@ -131,217 +99,33 @@ def main():
         if args.input is not None and frame is None:
             break
 
-        # resize the frame to have a maximum width of 500 pixels (the
-        # less data we have, the faster we can process it), then convert
-        # the frame from BGR to RGB for dlib
-        frame = imutils.resize(frame, width = 500)
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Outputs is list of frame, each with detected boxes
+        outputs, image_infos = track.detect(img=frame)
+        # If no object is detected
+        online_targets = torch.Tensor([[-1, -1, -1, -1, -1, -1, -1]])
+        if outputs[0] is not None:
+            online_targets = track.track(outputs[0], [image_infos['height'], image_infos['width']], class_name=args.class_name)
 
-        # if the frame dimensions are empty, set them
-        if W is None or H is None:
-            (H, W) = frame.shape[:2]
-
-        # if we are supposed to be writing a video to disk, initialize
-        # the writer
-        if args["output"] is not None and writer is None:
-            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-            writer = cv2.VideoWriter(args["output"], fourcc, 30,
-                (W, H), True)
-
-        # initialize the current status along with our list of bounding
-        # box rectangles returned by either (1) our object detector or
-        # (2) the correlation trackers
-        status = "Waiting"
-        rects = []
-
-        # check to see if we should run a more computationally expensive
-        # object detection method to aid our byte_tracker
-        if totalFrames % args["skip_frames"] == 0:
-            # set the status and initialize our new set of object trackers
-            status = "Detecting"
-            trackers = []
-
-            # convert the frame to a blob and pass the blob through the
-            # network and obtain the detections
-            detections = track.detect(frame)
-
-            # loop over the detections
-            for i in np.arange(0, detections.shape[2]):
-                # extract the confidence (i.e., probability) associated
-                # with the prediction
-                confidence = detections[0, 0, i, 2]
-
-                # filter out weak detections by requiring a minimum
-                # confidence
-                if confidence > args["confidence"]:
-                    # extract the index of the class label from the
-                    # detections list
-                    idx = int(detections[0, 0, i, 1])
-
-                    # if the class label is not a person, ignore it
-                    if idx != 0:
-                        continue
-
-                    # compute the (x, y)-coordinates of the bounding box
-                    # for the object
-                    box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-                    (startX, startY, endX, endY) = box.astype("int")
-
-                    # construct a dlib rectangle object from the bounding
-                    # box coordinates and then start the dlib correlation
-                    # byte_tracker
-                    tracker = dlib.correlation_tracker()
-                    rect = dlib.rectangle(startX, startY, endX, endY)
-                    tracker.start_track(rgb, rect)
-
-                    # add the byte_tracker to our list of trackers so we can
-                    # utilize it during skip frames
-                    trackers.append(tracker)
-
-        # otherwise, we should utilize our object *trackers* rather than
-        # object *detectors* to obtain a higher frame processing throughput
-        else:
-            # loop over the trackers
-            for tracker in trackers:
-                # set the status of our system to be 'tracking' rather
-                # than 'waiting' or 'detecting'
-                status = "Tracking"
-
-                # update the byte_tracker and grab the updated position
-                tracker.update(rgb)
-                pos = tracker.get_position()
-
-                # unpack the position object
-                startX = int(pos.left())
-                startY = int(pos.top())
-                endX = int(pos.right())
-                endY = int(pos.bottom())
-
-                # add the bounding box coordinates to the rectangles list
-                rects.append((startX, startY, endX, endY))
-
-        # draw a horizontal line in the center of the frame -- once an
-        # object crosses this line we will determine whether they were
-        # moving 'up' or 'down'
-        cv2.line(frame, (0, H // 2), (W, H // 2), (0, 0, 0), 3)
-        cv2.putText(frame, "-Prediction border - Entrance-", (10, H - ((i * 20) + 200)),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
-
-        # use the centroid byte_tracker to associate the (1) old object
-        # centroids with (2) the newly computed object centroids
-        objects = ct.update(rects)
-
-        # loop over the tracked objects
-        for (objectID, centroid) in objects.items():
-            # check to see if a trackable object exists for the current
-            # object ID
-            to = trackableObjects.get(objectID, None)
-
-            # if there is no existing trackable object, create one
-            if to is None:
-                to = TrackableObject(objectID, centroid)
-
-            # otherwise, there is a trackable object so we can utilize it
-            # to determine direction
-            else:
-                # the difference between the y-coordinate of the *current*
-                # centroid and the mean of *previous* centroids will tell
-                # us in which direction the object is moving (negative for
-                # 'up' and positive for 'down')
-                y = [c[1] for c in to.centroids]
-                direction = centroid[1] - np.mean(y)
-                to.centroids.append(centroid)
-
-                # check to see if the object has been counted or not
-                if not to.counted:
-                    # if the direction is negative (indicating the object
-                    # is moving up) AND the centroid is above the center
-                    # line, count the object
-                    if direction < 0 and centroid[1] < H // 2:
-                        totalUp += 1
-                        date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-                        move_out.append(totalUp)
-                        out_time.append(date_time)
-                        to.counted = True
-
-                    # if the direction is positive (indicating the object
-                    # is moving down) AND the centroid is below the
-                    # center line, count the object
-                    elif direction > 0 and centroid[1] > H // 2:
-                        totalDown += 1
-                        date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-                        move_in.append(totalDown)
-                        in_time.append(date_time)
-                        # if the people limit exceeds over threshold, send an email alert
-                        if sum(total) >= args.conf_thre:
-                            cv2.putText(frame, "-ALERT: People limit exceeded-", (10, frame.shape[0] - 80),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
-
-                        to.counted = True
-                        # compute the sum of total people inside
-                        total = []
-                        total.append(len(move_in) - len(move_out))
-
-            # store the trackable object in our dictionary
-            trackableObjects[objectID] = to
-
-            # draw both the ID of the object and the centroid of the
-            # object on the output frame
-            text = "ID {}".format(objectID)
-            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            cv2.circle(frame, (centroid[0], centroid[1]), 4, (255, 255, 255), -1)
-
-        # construct a tuple of information we will be displaying on the frame
-        info_status = [
-        ("Exit", totalUp),
-        ("Enter", totalDown),
-        ("Status", status),
-        ]
-
-        info_total = [
-        ("Total people inside", ', '.join(map(str, total))),
-        ]
-
-        # display the output
-        for (i, (k, v)) in enumerate(info_status):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
-
-        for (i, (k, v)) in enumerate(info_total):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (265, H - ((i * 20) + 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-
-        # check to see if we should write the frame to disk
-        if writer is not None:
-            writer.write(frame)
+        # Annotate video with tracked objects
+        frame = track.annotate(frame, online_targets)
 
         # show the output frame
         cv2.imshow("Real-Time Monitoring/Analysis Window", frame)
         key = cv2.waitKey(1) & 0xFF
+
         # if the `q` key was pressed, break from the loop
         if key == ord("q"):
             break
-        # increment the total number of frames processed thus far and
-        # then update the FPS counter
-        totalFrames += 1
-        fps.update()
-
-        # initiate the timer
-        # automatic timer to stop the live stream (set to 8 hours/28800s)
-        end_time = time.time()
-        num_seconds = (end_time - start_time)
-        if num_seconds > 28800:
-            break
-
-    # stop the timer and display FPS information
-    fps.stop()
-    logger.info("Elapsed time: {:.2f}".format(fps.elapsed()))
-    logger.info("Approx. FPS: {:.2f}".format(fps.fps()))
 
     # close any open windows
     cv2.destroyAllWindows()
+    track.unload()
 
 
 if __name__ == "__main__":
-    main()
+    # execution start time
+    start_time = time.time()
+    # setup logger
+    logging.basicConfig(level=logging.INFO, format="[INFO] %(message)s")
+    logger = logging.getLogger(__name__)
+    main(logger)
--- tracking.py
+++ tracking.py
@@ -1,40 +1,83 @@
 import numpy as np
 import torch
-import yaml
-from utils import data, boxes
+import os
+import cv2
+
+from utils import boxes, torch_utils
+from supervision.detection.core import Detections
+import supervision as sv
+from utils.data import check_img_size, class2id, letterbox
 
 
 class Tracking:
-    def __init__(self, model, tracker, polygons, args):
+    def __init__(self, logger, args, video_info, model=None, tracker=None):
         self.model = model
         self.tracker = tracker
-        self.polygons = polygons
+        self.video_info = video_info
         self.setting = args
+        self.logger = logger
 
-    # def load_model(self, weights_path, classes, device='cpu'):
-    #     with torch.no_grad():
-    #         self.device = select_device(device)
-    #         self.model = attempt_load(weights_path, device=self.device)
-    #
-    #         if device != "cpu":
-    #             self.model.half()
-    #             self.model.to(self.device).eval()
-    #
-    #         stride = int(self.model.stride.max())
-    #         self.img_size = check_img_size()
-    #         self.classes = yaml.load(open(classes), Loader=yaml.SafeLoader)["classes"]
+    def load_model(self, weights_path, classes, device='cpu'):
+        with torch.no_grad():
+            self.device = torch_utils.select_device(device)
+            if device != "cpu":
+                self.model.half()
+                self.model.to(self.device).eval()
+                self.logger.info("loading checkpoint")
+                ckpt = torch.load(weights_path, map_location=device)
+                # load the model state dict
+                self.model.load_state_dict(ckpt["model"])
+                self.logger.info("loaded checkpoint done.")
+
+            stride = int(self.model.stride.max())
+            self.image_size_wh = check_img_size(self.video_info.resolution_wh, stride=stride)
+            self.classes = class2id(classes_path=classes)
 
     def unload(self):
         if self.device.type != "cpu":
             torch.cuda.empty_cache()
 
-    def set(selfm, **config):
+    def set(self, **config):
         pass
+
+    def init_drawer(self, polygons):
+        colors = sv.ColorPalette.default()
+        
+        self.zones = [
+            sv.PolygonZone(
+                polygon=polygon,
+                frame_resolution_wh=self.video_info.resolution_wh
+            )
+            for polygon
+            in polygons
+        ]
+        self.zone_annotators = [
+            sv.PolygonZoneAnnotator(
+                zone=zone,
+                color=colors.by_idx(index),
+                thickness=6,
+                text_thickness=8,
+                text_scale=4
+            )
+            for index, zone
+            in enumerate(self.zones)
+        ]
+        self.box_annotators = [
+            sv.BoxAnnotator(
+                color=colors.by_idx(index),
+                thickness=4,
+                text_thickness=4,
+                text_scale=2
+            )
+            for index
+            in range(len(polygons))
+        ]
 
     def _preprocess_image(self, img):
         img0 = img.copy()
-        img = data.letterbox(img0, self.img_size, auto=self.img_size!=1280)
+        img = letterbox(img0, self.image_size_wh[::-1], auto=self.image_size_wh!=1280)
         img = img[:, :, ::-1]
+        img /= 255.0
         if self.setting.rgb_means:
             img -= (0.485, 0.456, 0.406),
         if self.setting.rgb_std:
@@ -42,13 +85,12 @@ class Tracking:
         img = img.transpose(2, 0, 1)
         img = np.ascontiguousarray(img)
         img = torch.from_numpy(img).to(self.device)
-        img = img.half() if self.device.type!="cpu" else img.float()
-        img /= 255.0
+        img = img.half() if self.device!="cpu" else img.float()
 
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
 
-        return img
+        return img0, img
 
     def _postprocess_image(self, pred):
         return boxes.postprocess(prediction=pred,
@@ -58,19 +100,71 @@ class Tracking:
                                  agnostic=False,
                                  )
 
-    def detect(self, img, track=False):
+    def detect(self, img):
+        img_info = {"id": 0}
+        if isinstance(img, str):
+            img_info["file_name"] = os.path.basename(img)
+            img = cv2.imread(img)
+        else:
+            img_info["file_name"] = None
+
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+
         with torch.no_grad():
+            # org_img: [w, h, 3]
+            # img: [1, 3, w, h]
             org_img, img = self._preprocess_image(img)
+            # Model outputs: [B, n_anchors, detect_attrs]
+            # Detections attrs ordered as (x1, y1, x2, y2, obj_conf, class_1, class_2, ...)
             pred = self.model(img)
+            # Post-process results ordered as (x1, y1, x2, y2, score, class)
             pred = self._postprocess_image(pred)
-            raw_detection = np.empty((0, 6), float)
 
-            for det in pred:
-                if len(det) > 0:
-                    det[:, :4] = boxes.scale_coords(img.shape[2:], det[:, :4], org_img.shape).round()
-                    for *xyxy, conf, cls in reversed(det):
-                        raw_detection = np.concatenate((raw_detection, [
-                            [int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3]), round(float(conf), 2), int(cls)]]))
+        return pred, img_info
 
-            # raw_detection = self.tracker.update(raw_detection)
+    def track(self, preds, class_name:str=None):
+        '''
+
+        :param preds: Tensor [n_anchors, 6]
+        :param image_shape: [height, weight]
+        :param class_id: provide if only track a specific class. E.g. 0: "person"
+        :return: [n_anchors, x1, y1, x2, y2, score, id, class]
+        '''
+        # Track a specific class
+        if class_name:
+            preds = preds[preds[5]==self.classes[class_name]]
+
+        # Return list of STrack objects
+        online_targets = self.tracker.update(preds, self.image_size_wh, self.image_size_wh)
+
+        results = torch.zeros((0, 6))
+
+        for i, t in enumerate(online_targets):
+            tlwh = t.tlwh
+            xyxy = t.xyxy
+            tid = t.track_id
+            vertical = tlwh[2] / tlwh[3] > self.setting.aspect_ratio_thresh
+            if tlwh[2] * tlwh[3] > self.setting.min_box_area and not vertical:
+                pred = torch.Tensor([[*xyxy, t.score, tid, t.class_id]])
+                results = torch.cat([results, pred], dim=0)
+
+        return results
+
+    def annotate(self, frame, results):
+        detections = Detections.from_yolox(results)
+
+        for zone, zone_annotator, box_annotator in zip(self.zones, self.zone_annotators, self.box_annotators):
+            mask = zone.trigger(detections=detections)
+            detections_filtered = detections[mask]
+            frame = box_annotator.annotate(scene=frame, detections=detections_filtered, skip_label=True)
+            frame = zone_annotator.annotate(scene=frame)
+
+        return frame
+
+
+
+
 
